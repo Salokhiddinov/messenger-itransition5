7bc5c6605e749e41488363bcb74b224e9c32970f
Salokhiddinov <nomonsalakhiddinov2002@gmail.com>
2022-09-07 01:46:42 +0500

    feat: add search

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -6,19 +6,13 @@ function SendMessage() {
   const inputTitle = useRef();
   let inputReciever = useRef();
   const [hints, setHints] = useState([]);
+  const [search, setSearch] = useState([]);
   const [showHints, setShowHints] = useState(false);
-  let temp = 0;
 
   async function loadHints(e) {
-    e.preventDefault();
-    setShowHints(true);
     try {
       let tempArr = [];
-      console.log("tempArr" + tempArr);
-
       const res = await axios.get("/api/usernames");
-      console.log("res" + res);
-
       for (let i = 0; i < res.data.length; i++) {
         let data = {
           id: i,
@@ -27,12 +21,19 @@ function SendMessage() {
         tempArr.push(data);
       }
       setHints(tempArr);
-      // hints(true);
-      temp++;
     } catch (err) {
       alert("Something went wrong");
       console.log(err);
     }
+  }
+  function searchReciever() {
+    let tempArr = [];
+    for (let i = 0; i < hints.length; i++) {
+      if (hints[i].reciever.includes(inputReciever.current.value)) {
+        tempArr.push(hints[i]);
+      }
+    }
+    setSearch(tempArr);
   }
   function setReciever(value) {
     inputReciever.current.value = value;
@@ -46,9 +47,9 @@ function SendMessage() {
     const enteredReciever = inputReciever.current.value;
 
     if (
-      enteredMessage.trim === "" ||
-      enteredTitle.trim === "" ||
-      enteredReciever.trim === ""
+      enteredMessage.trim() === "" ||
+      enteredTitle.trim() === "" ||
+      enteredReciever.trim() === ""
     ) {
       alert("Fields cannot be empty! Please, check your inputs");
       return false;
@@ -67,18 +68,16 @@ function SendMessage() {
       alert("Please check your username and password");
       return false;
     }
-    setShowHints(false);
     inputMessage.current.value = "";
     inputTitle.current.value = "";
     inputReciever = "";
   };
-  function toggleHints() {
-    setShowHints(!showHints);
-  }
+  console.log(hints);
+
   useEffect(() => {
     loadHints();
     // eslint-disable-next-line
-  }, [temp]);
+  }, []);
   return (
     <div>
       <form className="form-message">
@@ -91,50 +90,26 @@ function SendMessage() {
             <input
               ref={inputReciever}
               className="form-control"
-              onChange={loadHints}
-              // onClick={() => setShowHints(true)}
+              onChange={(e) => {
+                if (e.target.value.length > 0) {
+                  setShowHints(true);
+                  searchReciever();
+                }
+              }}
             />
-            {!showHints ? (
-              <button onClick={toggleHints} className="btn btn-secondary">
-                Show Hints 👇
-              </button>
-            ) : null}
-            {showHints ? (
-              <button onClick={toggleHints} className="btn btn-secondary">
-                Hide Hints 🔙
-              </button>
-            ) : null}
-            {showHints ? (
-              <ul lcassName="list-group">
-                {hints.map((hint) => (
+            {showHints && (
+              <ul className="list-group">
+                {search.map((s) => (
                   <li
-                    key={hint.id}
+                    key={s.id}
                     className="list-group-item"
-                    onClick={() => setReciever(hint.reciever)}
+                    onClick={() => setReciever(s.reciever)}
                   >
-                    {hint.reciever}
+                    {s.reciever}
                   </li>
                 ))}
               </ul>
-            ) : null}
-            {/* <div className="search_select_box">
-              <select className='form-select' >
-              <option defaultValue="selected" disabled>Select User</option>
-                {hints.map((hint) => (
-                  <option
-                    key={hint.id}
-                    onSelect={() => setReciever(hint.reciever)}
-                    value={hint.reciever}
-                  >
-                    {hint.reciever}
-                  </option>
-                ))}
-              </select>
-            </div> */}
-
-            {/* {showHints ? hints.map((hint) => (
-                <li key={hint.id} onClick={() => setReciever(hint.reciever)}>{hint.reciever}</li>
-              )): null} */}
+            )}
             <div className="list-group"></div>
             <label htmlFor="message" className="label">
               Title
